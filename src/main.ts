fb3539065009f7bdbdb39c69ba31ab16501db94c
fesuty <utan3289@gmail.com>
2026-04-26 17:28:51 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,67 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Stats from "three/addons/libs/stats.module.js";
+import GUI from "lil-gui";
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  100,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
-  100,
+  1000,
 );
 camera.position.z = 1.5;
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.outputColorSpace = THREE.SRGBColorSpace;
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
+new OrbitControls(camera, renderer.domElement);
+
+//BLOCK
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial({ wireframe: true });
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//fpsとか
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+//gui
+const gui = new GUI();
+
+const initial = {
+  reset: () => {
+    cube.rotation.x = 0;
+    cube.rotation.y = 0;
+    cube.rotation.z = 0;
+    gui.updateDisplay();
+  },
+};
+gui.add(initial, "reset").name("Reset");
+const cubeFolder = gui.addFolder("Cube");
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.open();
+
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.open();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  stats.update();
+}
+animate();
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshNormalMaterial({ wireframe: false });
-
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  controls.update();
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-animate();
